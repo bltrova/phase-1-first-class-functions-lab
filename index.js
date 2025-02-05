9d75da5e77d470e1acbe08ce8b31c612fb431d13
Bruno Trova <bltrova@gmail.com>
2025-02-05 11:27:14 -0800

    first-class-functions-lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+// declare a variable to returnFirstDriver as a function with one argument
+// return that argument using a non-destructive method with only the first two names
+
+const returnFirstTwoDrivers = (drivers) => {
+  return drivers.slice(0,2)
+}
+
+const returnLastTwoDrivers = (drivers) => {
+  return drivers.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(ride) {
+  return function(fare) {
+    return fare * ride;
+  }
+}
+const fareDoubler = createFareMultiplier(2);
+  console.log(fareDoubler(10));
+
+const fareTripler = createFareMultiplier(3)
+  console.log(fareTripler(30))
+
+const selectDifferentDrivers = function(drivers, otherFunction) {
+  return otherFunction(drivers);
+}
